a54bde564585c50275476b908d28cbec0745e9d3
Парников Алексей <a.parnikov@petrosoft.su>
2023-02-21 18:24:31 +0300

    Add TO and FROM byte-int convert of shpere index

--- Assets/Network/CustomProperties/BitSphere.cs
+++ Assets/Network/CustomProperties/BitSphere.cs
@@ -23,7 +23,7 @@ namespace com.czeeep.network {
         [SerializeField]
         public byte amount;
 
-        static Dictionary<byte, int> SphereComparisom = new Dictionary<byte, int>() {
+        static Dictionary<byte, int> SphereToByteComparisom = new Dictionary<byte, int>() {
             {0, 0 }, //None
             {1, 1 }, //life
             {2, 2 }, //fire
@@ -33,6 +33,18 @@ namespace com.czeeep.network {
             {6, 32 }, //razor
             {7, 64 }, //dark
             {8, 1024 }  //shield
+        };
+
+        static Dictionary<int, byte> ByteToSphereComparisom = new Dictionary<int, byte>() {
+            {0, 0 }, //None
+            {1, 1 }, //life
+            {2, 2 }, //fire
+            {4, 3 }, //water
+            {8, 4 }, //earth
+            {16, 5 }, //freeze
+            {32, 6 }, //razor
+            {64, 7 }, //dark
+            {1024, 8 }  //shield
         };
 
         public static byte[] ConvertSphere(int sphereId, int sphereType, int amount, Vector3 position) {
