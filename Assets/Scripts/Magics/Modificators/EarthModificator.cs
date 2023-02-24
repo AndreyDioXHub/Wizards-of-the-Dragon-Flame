40e225579489e66bd544507f691e118bc3222716
андрей огребо <ogreboandrey@yandex.ru>
2023-02-24 10:57:31 +0300

    stun

--- Assets/Scripts/Magics/Modificators/EarthModificator.cs
+++ Assets/Scripts/Magics/Modificators/EarthModificator.cs
@@ -17,11 +17,35 @@ namespace com.czeeep.spell.modificator
             _element.UpdateInfo(_info.key, _info.power, 1);
             //DoDamage();
             MagicModel.Instance.AddModificator("stun", _info.power);
-            DestroyModificator();
+            //DestroyModificator();
+            //стан не всегда проходит и не всегда урон
             //DoUpdatedDamage();
             //DoDamage();
 
         }
+        public override int CheckCancel(string sphere, int power, out bool isCancel)
+        {
+            base.CheckCancel(sphere, power, out isCancel);
+
+            int incomingPowerleft = power;
+
+            if (sphere == SpheresElements.none.ToString())
+            {
+                incomingPowerleft = (power - _info.power) <= 0 ? 0 : power - _info.power;
+
+                _info.power -= power;
+                isCancel = true;
+
+                if (_info.power <= 0)
+                {
+                    DestroyModificator();
+                }
+            }
+
+            _element.UpdateInfo(_info.key, _info.power, 1);
+            return incomingPowerleft;
+        }
+
         public override void DoDamage()
         {
             base.DoDamage();
