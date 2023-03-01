96a17025ba78172909dd1cca54ca8affd67a6839
Алексей Парников <cyraxchel@list.ru>
2023-03-01 23:22:34 +0300

    Test destroy mine

---
+++ Assets/OutWorldView/scripts/MineDemo.cs
@@ -0,0 +1,20 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            //PhotonNetwork.GetPhotonView()
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+}
