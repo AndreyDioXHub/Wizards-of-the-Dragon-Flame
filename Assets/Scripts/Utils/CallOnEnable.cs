06544690a8bdc8be59680183c0e448233613dae3
Парников Алексей <a.parnikov@petrosoft.su>
2023-03-07 01:35:03 +0300

    Add utils scripts

---
+++ Assets/Scripts/Utils/CallOnEnable.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using System;
+
+public class CallOnEnable : MonoBehaviour {
+    public UnityEvent EnableEvent;
+    public UnityEvent DisableEvent;
+    public UnityEvent StartEvent;
+
+    public UnityEvent OnDelayEnable;
+    public UnityEvent OnDelayStart;
+    [Tooltip("Seconds to delay event after start")]
+    public float DelaySeconds = 1.0f;
+
+    bool inited = false;
+    
+    public void OnEnable() {
+        EnableEvent.Invoke();
+        StartCoroutine(OnEnableDelay());
+    }
+    private IEnumerator OnEnableDelay()
+    {
+        yield return new WaitForSeconds(DelaySeconds);
+        OnDelayEnable.Invoke();
+    }
+
+    public void OnDisable() {
+        
+        DisableEvent.Invoke(); 
+    }
+
+    public void Start() {
+        inited = true;
+        StartEvent.Invoke();
+        StartCoroutine(StartDelay());
+    }
+
+    private IEnumerator StartDelay() {
+        yield return new WaitForSeconds(DelaySeconds);
+        OnDelayStart.Invoke();
+    }
+}
---
+++ Assets/Scripts/Utils/ObjectSwitch.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+namespace com.cyraxchel.utils {
+    public class ObjectSwitch : MonoBehaviour {
+
+        public UnityEvent<int> OnObjectSwitched;
+
+        public GameObject[] ObjectsPool;
+
+        public int FirstEnabled = -1;
+
+        public int CurrentObjectIndex = -1;
+
+        [Tooltip("Reset current state to start after reenable component")]
+        public bool ResetOnEnable = false;
+        bool inited = false;
+
+        void Awake() {
+            if (FirstEnabled > -1) {
+                EnableObject(FirstEnabled);
+            }
+            inited = true;
+        }
+        /// <summary>
+        /// Переключение на новый объект
+        /// </summary>
+        /// <param name="idEnabled"></param>
+        public void EnableObject(int idEnabled) {
+            for (int i = 0; i < ObjectsPool.Length; i++) {
+                ObjectsPool[i].SetActive(false);
+            }
+            if (idEnabled < ObjectsPool.Length && idEnabled > -1) {
+                ObjectsPool[idEnabled].SetActive(true);
+            }
+            CurrentObjectIndex = idEnabled;
+            OnObjectSwitched.Invoke(CurrentObjectIndex);
+
+        }
+
+        private void OnEnable() {
+            if (ResetOnEnable && inited && (FirstEnabled > -1)) {
+                EnableObject(FirstEnabled);
+            }
+        }
+        /// <summary>
+        /// Загрузка указанной сцены
+        /// </summary>
+        /// <param name="levelID"></param>
+        public void LoadLevel(int levelID) {
+            SceneManager.LoadScene(levelID);
+        }
+
+        public void Next() {
+            if (CurrentObjectIndex < ObjectsPool.Length) CurrentObjectIndex++;
+            EnableObject(CurrentObjectIndex);
+        }
+
+        public void Previous() {
+            if (CurrentObjectIndex > 0) CurrentObjectIndex--;
+            EnableObject(CurrentObjectIndex);
+        }
+    }
+}
